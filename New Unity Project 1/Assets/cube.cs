f5900bd3395f6e2c98ce105b7a6f3724f3aed12e
tinahk <tinakvist34@gmail.com>
2017-02-21 09:05:48 +0100

    Add unity test game

---
+++ New Unity Project 1/Assets/cube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour {
+
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+		moveSpeed = 10f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		//X,Y,Z
+		transform.Translate (Input.GetAxis("Horizontal") * Time.deltaTime, 0f, Input.GetAxis("Vertical"));
+
+
+	}
+}
