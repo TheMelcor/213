c95a56a3d1c41b708a81e2de5db7a9502912b190
TheMelcor <hakonme@online.no>
2017-04-17 11:41:52 +0200

    Working on opening door on trigger

---
+++ 213-game/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var Take 001 : Animation;
+        var DoorObject : GameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- 213-game/Assets/Scripts/PlayerController.cs
+++ 213-game/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 	void FixedUpdate(){
-		float moveX = Input.GetAxis ("Horizontal");
-		float moveZ = Input.GetAxis ("Vertical");
+		float moveX = Input.GetAxis ("Vertical");
+		float moveZ = Input.GetAxis ("Horizontal");
 		Vector3 movement = new Vector3 (moveX, 0.0f, moveZ);
 		GetComponent<Rigidbody>().velocity = movement;
 
