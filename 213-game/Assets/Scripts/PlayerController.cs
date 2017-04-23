5a8a7dc4c2f39172967e5c55c4af39955923ee3c
tinahk <tinakvist34@gmail.com>
2017-04-23 19:35:50 +0200

    Added new scene/level in game

--- 213-game/Assets/Scripts/PlayerController.cs
+++ 213-game/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour
-{
+{	
+
 	void FixedUpdate(){
 		float moveX = Input.GetAxis ("Vertical");
 		float moveZ = Input.GetAxis ("Horizontal");
@@ -11,5 +13,18 @@ public class PlayerController : MonoBehaviour
         Animation an;
         an = GetComponent<Animation>();
         an.Play();
+
 	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+
+		}
+
+	}
+
+
+	
 }
